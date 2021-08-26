0ad08ed40962f95af698a139841bc64ef4f3f72b
李思奇 <lisiqi@lisiqideMacBook-Pro.local>
2021-08-26 21:36:38 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+ <div id="nav">
+    <router-link to="/admin">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+   <router-view/>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
+<style lang='scss'>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/admin.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'admin',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang='scss'>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
